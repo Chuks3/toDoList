b876191782709368ff7b435ea5cf29e1bb334d68
Chuks <chuksonyekams@gmail.com>
2024-05-22 23:48:32 +0100

    Test

--- script.js
+++ script.js
@@ -1,35 +1,63 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("listContainer");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!");
+row.addEventListener('submit', (e) => {
+    var li = document.createElement("li");
+    var b = document.createElement("b");
+    var input = document.createElement("input");
+
+    e.preventDefault();
+    if(!inputBox.value){
+        alert("Please add to your list!");
     }
     else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+
+        input.type = "text";
+        input.value = inputBox.value;
+        input.setAttribute('readonly', 'readonly');
+        li.appendChild(input);
+
+        b.innerText = "\u003f";
+        li.appendChild(b);
+
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
+        span.innerText = "\u00d7";
         li.appendChild(span);
     }
     inputBox.value = '';
     saveData();
-}
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
+    listContainer.addEventListener("click", function(e){
+        if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+        }
+        else if(e.target.tagName === "INPUT"){
+            e.target.parentElement.classList.toggle("checked");
+            e.target.classList.toggle("checked");
+        }
+        else if(e.target.tagName === "LI"){
+            e.target.classList.toggle("checked");
+            e.target.firstElementChild.classList.toggle("checked");
+        }
+    }, false);
+
+    b.addEventListener('click', () => {
+        if(b.innerText.toLowerCase() == "\u003f"){
+            input.removeAttribute("readonly");
+            input.focus();
+            b.innerHTML = "s";
+        }
+        else{
+            input.setAttribute("readonly", "readonly");
+            b.innerText = "\u003f";
+        }
+    })
+
+})
 
 function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+    // localStorage.setItem("data", listContainer.innerHTML);
 }
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
