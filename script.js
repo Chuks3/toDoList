61093b1aade0d70b6cb90fdacfb2c1d0538d3415
Chuks <chuksonyekams@gmail.com>
2024-06-04 23:05:45 +0100

    Added an array to save data.

--- script.js
+++ script.js
@@ -1,86 +1,92 @@
-// Getting elements from the form
+// Getting data from the form
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("listContainer");
 
 // Create array to store tasks
-var lists = [];
+const lists = [];
 
-    row.addEventListener('submit', (e) => {
-    // Creating HTML elements for tasks with JS
-    var li = document.createElement("li");
-    var b = document.createElement("b");
-    var input = document.createElement("input");
-
-
-        // Preventing refresh of page upon submit
+row.addEventListener('submit', e => {
+    // Prevents page from reloading
     e.preventDefault();
-    if(!inputBox.value){
-        // Making sure input field !empty
-        alert("Please add to your list!");
+
+    // Making sure input field is !empty
+    if (inputBox.value.trim() === '') {
+        alert("Please add to your list!!!")
     }
     else{
+        // Adding data to bottom of the array
+        lists.push(inputBox.value);
 
-        // Adding JS element to page
+        // Clearing input field & displaying the list
+        inputBox.value = '';
+        showList();
+    }
+})
+
+// Making the showList function
+function showList() {
+    clearList(listContainer);
+
+    // Displaying each element with a loop
+    lists.forEach(list => {
+        // Creating, initializing, and adding an li element to the listContainer
+        const li = document.createElement('li');
         listContainer.appendChild(li);
 
-        // Creating, initializing, and adding input box to li element
-        input.type = "text";
-        input.value = inputBox.value;
+        // Creating, initializing, and adding an input to li element
+        const input = document.createElement('input');
         input.setAttribute('readonly', 'readonly');
+        input.value = list;
         li.appendChild(input);
 
+        // Adding checkbox functionalities
+        listContainer.addEventListener("click", e =>{
+            if(e.target.tagName === "INPUT"){
+                e.target.parentElement.classList.toggle("checked");
+                e.target.classList.toggle("checked");
+            }
+            else if(e.target.tagName === "LI"){
+                e.target.classList.toggle("checked");
+                e.target.firstElementChild.classList.toggle("checked");
+            }
+        }, false);
+        
         // Creating, initializing, and adding edit button to li element
+        const b = document.createElement('i');
         b.innerText = '\u270E';
         li.appendChild(b);
+        
+        // Adding functionality to edit button using an if statement
+        b.addEventListener('click', () => {
+            if(b.innerText.toLowerCase() == "\u270E"){
+                input.removeAttribute("readonly");
+                b.classList.add("far", "fa-save");
+                input.focus();
+                b.innerHTML = '';
+            }
+            else{
+                lists.splice(lists.indexOf(list), 1, input.value);
+                input.setAttribute("readonly", "readonly");
+                b.classList.remove("far", "fa-save");
+                b.innerText = '\u270E';
+            }
+        })
 
-        // Creating and adding delete button to li
-        let span = document.createElement("span");
+        // Creating and adding delete button to li element
+        const span = document.createElement("span");
         span.innerText = "\u00d7";
         li.appendChild(span);
 
-        // Clearing and saving input box values upon submittion
-        saveData();
-        inputBox.value = '';
-    }
-
-    // Adding functionalities to buttons with if statements
-    listContainer.addEventListener("click", function(e){
-        // Delete button
-        if(e.target.tagName === "SPAN"){
+        span.addEventListener('click', (e) => {
+            lists.splice(lists.indexOf(list), 1);
             e.target.parentElement.remove();
-        }
-        // Input box
-        else if(e.target.tagName === "INPUT"){
-            e.target.parentElement.classList.toggle("checked");
-            e.target.classList.toggle("checked");
-        }
-        // Check-box
-        else if(e.target.tagName === "LI"){
-            e.target.classList.toggle("checked");
-            e.target.firstElementChild.classList.toggle("checked");
-        }
-    }, false);
-
-    // Adding functionality to edit button using an if statement
-    b.addEventListener('click', () => {
-        if(b.innerText.toLowerCase() == "\u270E"){
-            input.removeAttribute("readonly");
-            input.focus();
-            b.innerHTML = '\u1f4be';
-        }
-        else{
-            input.setAttribute("readonly", "readonly");
-            b.innerText = "\u270E";
-        }
+        })
     })
+}
 
-    // Adding data to the array
-    function saveData(){
-        lists.push(inputBox.value);
+function clearList(element) {
+    while (element.firstChild){
+        element.removeChild(element.firstChild);
     }
-    // Displaying the array, but its !working
-    function showTask(){
-        console.log(lists);
-    }
-    showTask();
-})
+}
+showList();
